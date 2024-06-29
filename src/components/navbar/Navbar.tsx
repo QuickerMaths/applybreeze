d9ec4acc2592a2c0130ef65e21f2c3fe9252789f
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-06-29 20:34:53 +0200

    added modetoggle

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,22 +1,30 @@
-import React from 'react';
-import { TypographyH3 } from '../typography/typography';
-import { Button } from '../ui/button';
+import React from "react";
+import { TypographyH3 } from "../typography/typography";
+import { Button } from "../ui/button";
+import { ModeToggle } from "../mode-toggle/mode-toggle";
 
 export default function Navbar() {
- return (
-    <div className="flex items-center justify-between px-5 py-3 bg-foreground dark:bg-foreground">
-        <TypographyH3 className='text-secondary'>ApplyBreeze</TypographyH3>
-        <nav>
-            <ul className="flex items-center space-x-5">
-                <li>
-                    <a href="#" className="text-secondary">Jobs</a>
-                </li>
-                <li>
-                    <a href="#" className="text-secondary">Applications</a>
-                </li>
-            </ul>
-        </nav>
+  return (
+    <div className="flex items-center justify-between bg-foreground px-5 py-3 dark:bg-foreground">
+      <TypographyH3 className="text-secondary">ApplyBreeze</TypographyH3>
+      <nav>
+        <ul className="flex items-center space-x-5">
+          <li>
+            <a href="#" className="text-secondary">
+              Jobs
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-secondary">
+              Applications
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className="flex items-center justify-center gap-5">
+        <ModeToggle />
         <Button>SignIn</Button>
+      </div>
     </div>
   );
 }

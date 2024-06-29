685783a25f71dd08cfcfced8394d04f2d3d3df34
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-06-29 17:57:55 +0200

    added jobs and application links to navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { TypographyH3 } from '../typography/typography';
+import { Button } from '../ui/button';
 
 export default function Navbar() {
  return (
     <div className="flex items-center justify-between px-5 py-3 bg-foreground dark:bg-foreground">
         <TypographyH3 className='text-secondary'>ApplyBreeze</TypographyH3>
-        <button>SignIn</button>
+        <nav>
+            <ul className="flex items-center space-x-5">
+                <li>
+                    <a href="#" className="text-secondary">Jobs</a>
+                </li>
+                <li>
+                    <a href="#" className="text-secondary">Applications</a>
+                </li>
+            </ul>
+        </nav>
+        <Button>SignIn</Button>
     </div>
   );
 }

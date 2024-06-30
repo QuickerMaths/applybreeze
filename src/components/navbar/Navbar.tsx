cf27589b249007d43b4c372ac5c6e1a23667507e
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-06-30 14:11:05 +0200

    implemented mobilemenu

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,12 +3,13 @@ import { TypographyH3 } from "../typography/typography";
 import { Button } from "../ui/button";
 import { ModeToggle } from "../mode-toggle/mode-toggle";
 import Link from "next/link";
+import { MobileMenu } from "../mobile-menu/mobile-menu";
 
 export default function Navbar() {
   return (
     <div className="flex items-center justify-between bg-foreground px-5 py-3 dark:bg-foreground">
       <TypographyH3 className="text-secondary">ApplyBreeze</TypographyH3>
-      <nav>
+      <nav className="hidden sm:block">
         <ul className="flex items-center space-x-5">
           <li>
             <Link href="/" className="text-secondary">
@@ -28,8 +29,9 @@ export default function Navbar() {
         </ul>
       </nav>
       <div className="flex items-center justify-center gap-5">
-        <ModeToggle />
         <Button>SignIn</Button>
+        <ModeToggle className="hidden sm:flex" />
+        <MobileMenu className="dark:bg-white sm:hidden" />
       </div>
     </div>
   );

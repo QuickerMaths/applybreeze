c86354dd6ce9a179e7280e7f1d4f94e2c74965dc
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-07-06 14:45:33 +0200

    added empty route

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,47 +6,47 @@ import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 import { Button } from "../ui/button";
 
 export default function Navbar() {
-    return (
-        <div className="flex items-center justify-between bg-foreground px-5 py-3 dark:bg-foreground">
-            <TypographyH3 className="text-secondary">ApplyBreeze</TypographyH3>
-            <nav className="hidden sm:block">
-                <ul className="flex items-center space-x-5">
-                    <li>
-                        <Link href="/" className="text-secondary">
-                            Home
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/jobs" className="text-secondary">
-                            Find Jobs
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/saved-jobs" className="text-secondary">
-                            Saved Jobs
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/applications" className="text-secondary">
-                            Applications
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            <div className="flex items-center justify-center gap-5">
-                <SignedOut>
-                    <SignInButton>
-                        <Button className="text-black dark:bg-white" variant="outline">
-                            Sign In
-                        </Button>
-                    </SignInButton>
-                </SignedOut>
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
-                <ModeToggle className="hidden sm:flex" />
-                <MobileMenu className="dark:bg-white sm:hidden" />
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex items-center justify-between bg-foreground px-5 py-3 dark:bg-foreground">
+      <TypographyH3 className="text-secondary">ApplyBreeze</TypographyH3>
+      <nav className="hidden sm:block">
+        <ul className="flex items-center space-x-5">
+          <li>
+            <Link href="/" className="text-secondary">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/jobs" className="text-secondary">
+              Find Jobs
+            </Link>
+          </li>
+          <li>
+            <Link href="/saved-searches" className="text-secondary">
+              Saved Searches
+            </Link>
+          </li>
+          <li>
+            <Link href="/applications" className="text-secondary">
+              Applications
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="flex items-center justify-center gap-5">
+        <SignedOut>
+          <SignInButton>
+            <Button className="text-black dark:bg-white" variant="outline">
+              Sign In
+            </Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <ModeToggle className="hidden sm:flex" />
+        <MobileMenu className="dark:bg-white sm:hidden" />
+      </div>
+    </div>
+  );
 }

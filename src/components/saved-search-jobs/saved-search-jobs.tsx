fdf762f6a201ffd2d167e4f8661969adba277323
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-07-16 21:11:48 +0200

    removed unused imports

--- src/components/saved-search-jobs/saved-search-jobs.tsx
+++ src/components/saved-search-jobs/saved-search-jobs.tsx
@@ -1,146 +1,141 @@
 "use client";
 
 import {
-  useQueryClient,
-  useInfiniteQuery,
-  useQuery,
+    useInfiniteQuery,
+    useQuery
 } from "@tanstack/react-query";
 import { getPendingRequests } from "~/server/queries/request-queries";
 import { getSavedSearchJobs } from "~/server/queries/jobs-queries";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableFooter,
-  TableHead,
-  TableHeader,
-  TableRow,
+    Table,
+    TableBody,
+    TableCell,
+    TableFooter,
+    TableHead,
+    TableHeader,
+    TableRow,
 } from "~/components/ui/table";
 import { Button } from "../ui/button";
 import RequestLoader from "../request-loader/request-loader";
 import Link from "next/link";
-import { useRouter, usePathname } from "next/navigation";
 import useCreateQueryString from "~/hooks/useCreateQueryString";
 
 interface SavedSearchJobsProps {
-  userId: string;
-  savedSearchId: number;
+    userId: string;
+    savedSearchId: number;
 }
 
 export default function SavedSearchJobs({
-  userId,
-  savedSearchId,
+    userId,
+    savedSearchId,
 }: SavedSearchJobsProps) {
-  const queryClient = useQueryClient();
-  const router = useRouter();
-  const pathname = usePathname();
-  const createQueryString = useCreateQueryString();
-  const {
-    data: savedSearchJobsData,
-    hasNextPage,
-    isFetchingNextPage,
-    isFetching,
-    fetchNextPage,
-  } = useInfiniteQuery({
-    queryKey: ["savedSearchJobs", userId],
-    queryFn: ({ pageParam }: { pageParam: number }) =>
-      getSavedSearchJobs(savedSearchId, pageParam),
-    initialPageParam: 0,
-    getNextPageParam: (lastPage, _pages) => {
-      if (lastPage.length < 10) return undefined;
-      const lastId = lastPage[lastPage.length - 1]?.id;
-      return lastId;
-    },
-  });
+    const createQueryString = useCreateQueryString();
+    const {
+        data: savedSearchJobsData,
+        hasNextPage,
+        isFetchingNextPage,
+        isFetching,
+        fetchNextPage,
+    } = useInfiniteQuery({
+        queryKey: ["savedSearchJobs", userId],
+        queryFn: ({ pageParam }: { pageParam: number }) =>
+            getSavedSearchJobs(savedSearchId, pageParam),
+        initialPageParam: 0,
+        getNextPageParam: (lastPage, _pages) => {
+            if (lastPage.length < 10) return undefined;
+            const lastId = lastPage[lastPage.length - 1]?.id;
+            return lastId;
+        },
+    });
 
-  const { data: pendingRequestsData } = useQuery({
-    queryKey: ["requests", userId],
-    queryFn: async () => await getPendingRequests(userId),
-  });
+    const { data: pendingRequestsData } = useQuery({
+        queryKey: ["requests", userId],
+        queryFn: async () => await getPendingRequests(userId),
+    });
 
-  return (
-    <div className="mx-auto flex flex-col items-center justify-center">
-      {pendingRequestsData && pendingRequestsData.length > 0 && (
-        <>
-          <h2 className="mb-2 text-2xl font-bold text-primary dark:text-primary">
-            Pending requests
-          </h2>
-          <ul className="mb-2 flex w-full flex-col gap-y-2">
-            {pendingRequestsData.map((request) => (
-              <RequestLoader
-                key={request.id}
-                userId={userId}
-                request={request}
-                isInSavedSearchJobs
-              />
-            ))}
-          </ul>
-        </>
-      )}
-      {savedSearchJobsData?.pages ? (
-        <>
-          <Table className="block h-[700px] w-full overflow-y-scroll">
-            <TableHeader>
-              <TableRow>
-                <TableHead className="dark:text-white">Role</TableHead>
-                <TableHead className="dark:text-white">City</TableHead>
-                <TableHead className="dark:text-white">Country</TableHead>
-                <TableHead className="dark:text-white">Salary</TableHead>
-                <TableHead className="dark:text-white">Seniority</TableHead>
-                <TableHead className="dark:text-white">Url</TableHead>
-                <TableHead className="dark:text-white"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {savedSearchJobsData.pages.map((page) =>
-                page.map((job) => (
-                  <TableRow key={job.id}>
-                    <TableCell className="w-[10%]">{job.job.title}</TableCell>
-                    <TableCell>{job.job.city}</TableCell>
-                    <TableCell>{job.job.country}</TableCell>
-                    <TableCell>{job.job.salary ?? "N/A"}</TableCell>
-                    <TableCell>{job.job.seniorityLevel}</TableCell>
-                    <TableCell className="max-w-[100px] truncate">
-                      <a
-                        href={job.job.url ?? job.job.sourceUrl!}
-                        target="_blank"
-                      >
-                        {job.job.url ?? job.job.sourceUrl}
-                      </a>
-                    </TableCell>
-                    <TableCell>
-                      <Link
-                        href={`${savedSearchId}/job-details?${createQueryString({ jobId: job.id })}`}
-                      >
-                        <Button>Details</Button>
-                      </Link>
-                    </TableCell>
-                  </TableRow>
-                )),
-              )}
-            </TableBody>
-            <TableFooter>
-              <TableRow>
-                <TableCell colSpan={10}>
-                  {hasNextPage ? (
-                    <Button
-                      onClick={() => fetchNextPage()}
-                      className="mx-auto mt-2 block w-1/2"
-                      disabled={isFetchingNextPage || isFetching}
-                    >
-                      {isFetchingNextPage ? "Loading more..." : "Load more"}
-                    </Button>
-                  ) : (
-                    <p className="mt-2 text-center">No more results</p>
-                  )}
-                </TableCell>
-              </TableRow>
-            </TableFooter>
-          </Table>
-        </>
-      ) : (
-        <p>No jobs found</p>
-      )}
-    </div>
-  );
+    return (
+        <div className="mx-auto flex flex-col items-center justify-center">
+            {pendingRequestsData && pendingRequestsData.length > 0 && (
+                <>
+                    <h2 className="mb-2 text-2xl font-bold text-primary dark:text-primary">
+                        Pending requests
+                    </h2>
+                    <ul className="mb-2 flex w-full flex-col gap-y-2">
+                        {pendingRequestsData.map((request) => (
+                            <RequestLoader
+                                key={request.id}
+                                userId={userId}
+                                request={request}
+                                isInSavedSearchJobs
+                            />
+                        ))}
+                    </ul>
+                </>
+            )}
+            {savedSearchJobsData?.pages ? (
+                <>
+                    <Table className="block h-[700px] w-full overflow-y-scroll">
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="dark:text-white">Role</TableHead>
+                                <TableHead className="dark:text-white">City</TableHead>
+                                <TableHead className="dark:text-white">Country</TableHead>
+                                <TableHead className="dark:text-white">Salary</TableHead>
+                                <TableHead className="dark:text-white">Seniority</TableHead>
+                                <TableHead className="dark:text-white">Url</TableHead>
+                                <TableHead className="dark:text-white"></TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {savedSearchJobsData.pages.map((page) =>
+                                page.map((job) => (
+                                    <TableRow key={job.id}>
+                                        <TableCell className="w-[10%]">{job.job.title}</TableCell>
+                                        <TableCell>{job.job.city}</TableCell>
+                                        <TableCell>{job.job.country}</TableCell>
+                                        <TableCell>{job.job.salary ?? "N/A"}</TableCell>
+                                        <TableCell>{job.job.seniorityLevel}</TableCell>
+                                        <TableCell className="max-w-[100px] truncate">
+                                            <a
+                                                href={job.job.url ?? job.job.sourceUrl!}
+                                                target="_blank"
+                                            >
+                                                {job.job.url ?? job.job.sourceUrl}
+                                            </a>
+                                        </TableCell>
+                                        <TableCell>
+                                            <Link
+                                                href={`${savedSearchId}/job-details?${createQueryString({ jobId: job.id })}`}
+                                            >
+                                                <Button>Details</Button>
+                                            </Link>
+                                        </TableCell>
+                                    </TableRow>
+                                )),
+                            )}
+                        </TableBody>
+                        <TableFooter>
+                            <TableRow>
+                                <TableCell colSpan={10}>
+                                    {hasNextPage ? (
+                                        <Button
+                                            onClick={() => fetchNextPage()}
+                                            className="mx-auto mt-2 block w-1/2"
+                                            disabled={isFetchingNextPage || isFetching}
+                                        >
+                                            {isFetchingNextPage ? "Loading more..." : "Load more"}
+                                        </Button>
+                                    ) : (
+                                        <p className="mt-2 text-center">No more results</p>
+                                    )}
+                                </TableCell>
+                            </TableRow>
+                        </TableFooter>
+                    </Table>
+                </>
+            ) : (
+                <p>No jobs found</p>
+            )}
+        </div>
+    );
 }

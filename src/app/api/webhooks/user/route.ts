52b13ae65b70ad5ced221dc0b29ea4bb179a0d9a
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-07-21 18:17:11 +0200

    import update

--- src/app/api/webhooks/user/route.ts
+++ src/app/api/webhooks/user/route.ts
@@ -2,65 +2,65 @@ import { Webhook } from "svix";
 import { headers } from "next/headers";
 import type { WebhookEvent } from "@clerk/nextjs/server";
 import {
-  saveUser,
-  updateUser,
-  deleteUser,
-} from "~/server/queries/jobs-queries";
+    saveUser,
+    updateUser,
+    deleteUser,
+} from "~/server/queries/user-queries";
 
 export async function POST(req: Request) {
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+    const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
-  if (!WEBHOOK_SECRET) {
-    throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local",
-    );
-  }
+    if (!WEBHOOK_SECRET) {
+        throw new Error(
+            "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local",
+        );
+    }
 
-  const headerPayload = headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
+    const headerPayload = headers();
+    const svix_id = headerPayload.get("svix-id");
+    const svix_timestamp = headerPayload.get("svix-timestamp");
+    const svix_signature = headerPayload.get("svix-signature");
 
-  if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
-      status: 400,
-    });
-  }
+    if (!svix_id || !svix_timestamp || !svix_signature) {
+        return new Response("Error occured -- no svix headers", {
+            status: 400,
+        });
+    }
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const payload = await req.json();
-  const body = JSON.stringify(payload);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const payload = await req.json();
+    const body = JSON.stringify(payload);
 
-  const wh = new Webhook(WEBHOOK_SECRET);
+    const wh = new Webhook(WEBHOOK_SECRET);
 
-  let evt: WebhookEvent;
+    let evt: WebhookEvent;
 
-  try {
-    evt = wh.verify(body, {
-      "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
-      "svix-signature": svix_signature,
-    }) as WebhookEvent;
-  } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
-      status: 400,
-    });
-  }
+    try {
+        evt = wh.verify(body, {
+            "svix-id": svix_id,
+            "svix-timestamp": svix_timestamp,
+            "svix-signature": svix_signature,
+        }) as WebhookEvent;
+    } catch (err) {
+        console.error("Error verifying webhook:", err);
+        return new Response("Error occured", {
+            status: 400,
+        });
+    }
 
-  const eventType = evt.type;
-  const { id } = evt.data;
+    const eventType = evt.type;
+    const { id } = evt.data;
 
-  if (eventType === "user.created") {
-    await saveUser(evt);
-    return new Response("OK", { status: 201 });
-  } else if (eventType === "user.updated" && id) {
-    await updateUser(evt, id);
-    return new Response("OK", { status: 200 });
-  } else if (eventType === "user.deleted" && id) {
-    await deleteUser(id);
-    return new Response("OK", { status: 204 });
-  }
+    if (eventType === "user.created") {
+        await saveUser(evt);
+        return new Response("OK", { status: 201 });
+    } else if (eventType === "user.updated" && id) {
+        await updateUser(evt, id);
+        return new Response("OK", { status: 200 });
+    } else if (eventType === "user.deleted" && id) {
+        await deleteUser(id);
+        return new Response("OK", { status: 204 });
+    }
 
-  return new Response("Server internal error", { status: 500 });
+    return new Response("Server internal error", { status: 500 });
 }

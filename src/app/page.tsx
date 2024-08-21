9ac926b618f0c8b6e82d688bbdac937fbfa756b6
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-08-21 13:23:52 +0200

    implemented bar-chart prototype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import BarChartComponent from "~/components/bar-chart/bar-chart";
+
 export default function HomePage() {
-  return (
-    <main className="flex flex-col items-center justify-center bg-background dark:bg-background"></main>
-  );
+    return (
+        <main className="flex flex-col items-center justify-center bg-background dark:bg-background">
+            <BarChartComponent />
+        </main>
+    );
 }

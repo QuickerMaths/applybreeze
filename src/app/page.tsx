5b878b31e604dc6dba76e623b953c4ff68d4356b
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-08-22 18:26:13 +0200

    displayed analytics on the bar-chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,42 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import BarChartComponent from "~/components/bar-chart/bar-chart";
+import { getCurrentUserId } from "~/lib/getCurrentUser";
+import { getAnalytics } from "~/server/queries/analytics-queries";
 
-export default function HomePage() {
-    return (
-        <main className="flex flex-col items-center justify-center bg-background dark:bg-background">
-            <BarChartComponent />
-        </main>
-    );
+export default async function HomePage() {
+  const userId = await getCurrentUserId();
+
+  if (!userId) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        gcTime: 1000 * 60 * 60,
+      },
+    },
+  });
+
+  await queryClient.prefetchQuery({
+    queryKey: ["analytics", userId],
+    queryFn: async () => await getAnalytics(userId, 3),
+  });
+
+  return (
+    <main className="flex items-center justify-center gap-5 bg-background dark:bg-background">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <BarChartComponent userId={userId} />
+      </HydrationBoundary>
+    </main>
+  );
 }
--- src/components/bar-chart/bar-chart.tsx
+++ src/components/bar-chart/bar-chart.tsx
@@ -1,84 +1,95 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
 import { TrendingUp } from "lucide-react";
-import { Bar, BarChart, CartesianGrid, LabelList, XAxis } from "recharts";
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from "~/components/ui/card";
 import {
-    type ChartConfig,
-    ChartContainer,
-    ChartTooltip,
-    ChartTooltipContent,
+  type ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
 } from "~/components/ui/chart";
-const chartData = [
-    { month: "January", desktop: 186 },
-    { month: "February", desktop: 305 },
-    { month: "March", desktop: 237 },
-    { month: "April", desktop: 73 },
-    { month: "May", desktop: 209 },
-    { month: "June", desktop: 214 },
+import { getAnalytics } from "~/server/queries/analytics-queries";
+const artData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
 ];
 
 const chartConfig = {
-    desktop: {
-        label: "Desktop",
-        color: "hsl(var(--chart-1))",
-    },
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "hsl(var(--chart-2))",
+  },
 } satisfies ChartConfig;
 
-export default function BarChartComponent() {
-    return (
-        <Card>
-            <CardHeader>
-                <CardTitle>Bar Chart - Label</CardTitle>
-                <CardDescription>January - June 2024</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <ChartContainer config={chartConfig}>
-                    <BarChart
-                        accessibilityLayer
-                        data={chartData}
-                        margin={{
-                            top: 20,
-                        }}
-                    >
-                        <CartesianGrid vertical={false} />
-                        <XAxis
-                            dataKey="month"
-                            tickLine={false}
-                            tickMargin={10}
-                            axisLine={false}
-                            tickFormatter={(value) => value.slice(0, 3)}
-                        />
-                        <ChartTooltip
-                            cursor={false}
-                            content={<ChartTooltipContent hideLabel />}
-                        />
-                        <Bar dataKey="desktop" fill="var(--color-desktop)" radius={8}>
-                            <LabelList
-                                position="top"
-                                offset={12}
-                                className="fill-foreground"
-                                fontSize={12}
-                            />
-                        </Bar>
-                    </BarChart>
-                </ChartContainer>
-            </CardContent>
-            <CardFooter className="flex-col items-start gap-2 text-sm">
-                <div className="flex gap-2 font-medium leading-none">
-                    Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-                </div>
-                <div className="leading-none text-muted-foreground">
-                    Showing total visitors for the last 6 months
-                </div>
-            </CardFooter>
-        </Card>
-    );
+interface BarChartComponentProps {
+  userId: string;
+}
+
+export default function BarChartComponent({ userId }: BarChartComponentProps) {
+  const { data: analytics } = useQuery({
+    queryKey: ["analytics", userId],
+    queryFn: async () => await getAnalytics(userId, 3),
+  });
+
+  const chartData = analytics?.map((month) => {
+    return {
+      date: `${month.month}.${month.year}`,
+      searches: month.savedApplications,
+      applications: month.appliedApplications,
+    };
+  });
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Bar Chart - Multiple</CardTitle>
+        <CardDescription>January - June 2024</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <BarChart accessibilityLayer data={chartData}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="date"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="dashed" />}
+            />
+            <Bar dataKey="searches" fill="var(--color-desktop)" radius={4} />
+            <Bar dataKey="applications" fill="var(--color-mobile)" radius={4} />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-2 text-sm">
+        <div className="flex gap-2 font-medium leading-none">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="leading-none text-muted-foreground">
+          Showing total visitors for the last 6 months
+        </div>
+      </CardFooter>
+    </Card>
+  );
 }
